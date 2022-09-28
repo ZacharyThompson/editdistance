df2b4924ebec56cbd862156067bb0bf841903228
Zachary Thompson <zachthompson406@gmail.com>
2022-09-28 15:29:18 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    word1 = input("Enter word #1: ")
+    word2 = input("Enter word #2: ")
+    print(f"Word 1 is: {word1}")
+    print(f"Word 2 is: {word2}")
+    return
+
+if __name__ == "__main__":
+    main()
