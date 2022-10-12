59b3938bad82549b6441a4223b70f22ccabe7cd9
Zachary Thompson <zachthompson406@gmail.com>
2022-10-11 21:34:02 -0700

    Create EditDist class

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
+class EditDist:
+    def __init__(self, word1: str, word2: str):
+        self.word1 = word1
+        self.word2 = word2
+        self.matrix = [len(word1)][len(word2)]
+
+
 def main():
     word1 = input("Enter word #1: ")
     word2 = input("Enter word #2: ")
     print(f"Word 1 is: {word1}")
     print(f"Word 2 is: {word2}")
     return
+
 
 if __name__ == "__main__":
     main()
